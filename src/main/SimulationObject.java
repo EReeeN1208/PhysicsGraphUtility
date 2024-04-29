b862f5bdf7d78ecaafc664532f81a3d5d0a09877
EReeeN1208 <52812989+EReeeN1208@users.noreply.github.com>
2024-04-29 19:48:06 +0300

    Environment class

--- src/main/SimulationObject.java
+++ src/main/SimulationObject.java
@@ -1,13 +1,17 @@
 package main;
 
 public class SimulationObject {
+
+    String name;
+
     double radius;
     double cross_section_area;
     double drag_coefficient;
     double mass;
 
     //For non-spherical objects
-    public SimulationObject (double radius, double cross_section_area, double drag_coefficient, double mass) {
+    public SimulationObject (String name, double radius, double cross_section_area, double drag_coefficient, double mass) {
+        this.name = name;
         this.radius = radius;
         this.cross_section_area = cross_section_area;
         this.drag_coefficient = drag_coefficient;
@@ -15,7 +19,8 @@ public class SimulationObject {
     }
 
     //For spherical objects
-    public SimulationObject (double radius, double drag_coefficient, double mass) {
+    public SimulationObject (String name, double radius, double drag_coefficient, double mass) {
+        this.name = name;
         this.radius = radius;
         this.cross_section_area = radius * radius * Math.PI;
         this.drag_coefficient = drag_coefficient;
